8c37d1a261e9ddeff7bad4613f7e87b55747177b
SirMashedPotato <88062719+SirMashedPotato@users.noreply.github.com>
2022-08-10 09:53:36 +1200

    Melynx pilfer stat

---
+++ 1.3/Source/Mashed_Lynians/Mashed_Lynians/StatPart/StatPart_SkillOffset.cs
@@ -0,0 +1,41 @@
+﻿using Verse;
+using RimWorld;
+
+namespace Mashed_Lynians
+{
+    class StatPart_SkillOffset : StatPart
+    {
+        public override string ExplanationPart(StatRequest req)
+        {
+            Pawn pawn;
+            if ((pawn = (req.Thing as Pawn)) == null)
+            {
+                return null;
+            }
+            return skillDef.label.CapitalizeFirst() + " " + pawn.skills.GetSkill(skillDef).Level + " x " + factorPerLevel + ": " + this.StatFactor(pawn).ToStringPercent();
+        }
+
+        public override void TransformValue(StatRequest req, ref float val)
+        {
+            Pawn pawn;
+            if ((pawn = (req.Thing as Pawn)) == null)
+            {
+                return;
+            }
+            val += this.StatFactor(pawn);
+        }
+
+        public float StatFactor(Pawn pawn)
+        {
+            if (pawn.def == raceThingDef)
+            {
+                return factorPerLevel * (float)pawn.skills.GetSkill(skillDef).Level;
+            }
+            return 0f;
+        }
+
+        public float factorPerLevel = 0.3f;
+        public SkillDef skillDef;
+        public ThingDef raceThingDef;
+    }
+}
