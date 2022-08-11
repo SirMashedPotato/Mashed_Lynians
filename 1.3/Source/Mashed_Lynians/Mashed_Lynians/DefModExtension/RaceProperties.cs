85d167d5eae2a8f4a6c144c8abf317d5e8de08f2
SirMashedPotato <88062719+SirMashedPotato@users.noreply.github.com>
2022-08-11 13:27:16 +1200

    Felvine

---
+++ 1.3/Source/Mashed_Lynians/Mashed_Lynians/DefModExtension/RaceProperties.cs
@@ -0,0 +1,13 @@
+﻿using Verse;
+
+namespace Mashed_Lynians
+{
+    public class RaceProperties : DefModExtension
+    {
+        public bool canUseFelvine = true;
+        public static RaceProperties Get(Def def)
+        {
+            return def.GetModExtension<RaceProperties>();
+        }
+    }
+}
---
+++ 1.3/Source/Mashed_Lynians/Mashed_Lynians/Utility.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Verse;
+using RimWorld;
+
+namespace Mashed_Lynians
+{
+    public static class Utility
+    {
+        public static bool CanUseFelvine(Pawn pawn)
+        {
+            return CanUseFelvine(pawn as Thing);
+        }
+
+        public static bool CanUseFelvine(Thing thing)
+        {
+            RaceProperties props = RaceProperties.Get(thing.def);
+            return props != null && props.canUseFelvine;
+        }
+    }
+}
