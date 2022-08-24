dfd814fdbf971a515014903dd91693a46d6a85ec
SirMashedPotato <88062719+SirMashedPotato@users.noreply.github.com>
2022-08-25 08:53:56 +1200

    Gajalaka weapons

---
+++ 1.3/Source/Mashed_Lynians/Mashed_Lynians/Verb/Verb_ShootWithDurability.cs
@@ -0,0 +1,31 @@
+﻿using Verse;
+
+namespace Mashed_Lynians
+{
+    public class Verb_ShootWithDurability : Verb_Shoot
+    {
+        protected override bool TryCastShot()
+        {
+            if (base.TryCastShot())
+            {
+                if (base.EquipmentSource != null)
+                {
+                    base.EquipmentSource.HitPoints -= (int)(base.EquipmentSource.MaxHitPoints * 0.05f);
+                    if (base.EquipmentSource.HitPoints <= 0)
+                    {
+                        this.SelfConsume();
+                    }
+                }
+            }
+            return true;
+        }
+
+        private void SelfConsume()
+        {
+            if (base.EquipmentSource != null && !base.EquipmentSource.Destroyed)
+            {
+                base.EquipmentSource.Destroy(DestroyMode.Vanish);
+            }
+        }
+    }
+}
