cb6c4384adeac29f63a1ba793aa3bf979944ec40
SirMashedPotato <88062719+SirMashedPotato@users.noreply.github.com>
2022-08-31 07:29:03 +1200

    Shakalaka wanderer quest

---
+++ 1.3/Source/Mashed_Lynians/Mashed_Lynians/DefOf/FactionDefOf.cs
@@ -0,0 +1,16 @@
+﻿using RimWorld;
+
+namespace Mashed_Lynians
+{
+	[DefOf]
+	public static class FactionDefOf
+	{
+
+		static FactionDefOf()
+		{
+			DefOfHelper.EnsureInitializedInCtor(typeof(ChemicalDefOf));
+		}
+
+		//public static FactionDef Mashed_Lynian_NonPlayerShakalakaWanderers;
+	}
+}
---
+++ 1.3/Source/Mashed_Lynians/Mashed_Lynians/DefOf/PawnKindDefOf.cs
@@ -0,0 +1,17 @@
+﻿using Verse;
+using RimWorld;
+
+namespace Mashed_Lynians
+{
+	[DefOf]
+	public static class PawnKindDefOf
+	{
+
+		static PawnKindDefOf()
+		{
+			DefOfHelper.EnsureInitializedInCtor(typeof(PawnKindDefOf));
+		}
+
+		public static PawnKindDef Mashed_Lynian_ShakalakaWanderer;
+	}
+}
