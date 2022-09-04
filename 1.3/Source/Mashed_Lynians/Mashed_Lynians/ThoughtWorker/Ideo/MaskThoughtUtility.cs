13b3348abc7ec9103250b6e032ba3c4fe9bef0fb
SirMashedPotato <88062719+SirMashedPotato@users.noreply.github.com>
2022-09-05 08:52:38 +1200

    Ideo Memes redo

---
+++ 1.3/Source/Mashed_Lynians/Mashed_Lynians/ThoughtWorker/Ideo/MaskThoughtUtility.cs
@@ -0,0 +1,65 @@
+﻿using RimWorld;
+using Verse;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Mashed_Lynians
+{
+    public static class MaskThoughtUtility
+    {
+        public static bool Valid(ThingDef def) => ValidNow() && ValidPawn(def);
+
+        public static bool ValidNow()
+        {
+            return GenDate.DaysPassedSinceSettle < 15;
+        }
+
+        public static bool ValidPawn(ThingDef def)
+        {
+            RaceProperties rp = RaceProperties.Get(def);
+            return rp == null || !rp.isLynian;
+        }
+
+        public static bool WearingValidMask_Boaboa(Pawn p)
+        {
+            List<Apparel> wornApparel = p.apparel.WornApparel.Where(x => x.def.apparel.bodyPartGroups.Contains(BodyPartGroupDefOf.FullHead)).ToList();
+            foreach (Apparel ap in wornApparel)
+            {
+                ApparelProperties props = ApparelProperties.Get(ap.def);
+                if (props != null && props.isBoaboaMask)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static bool WearingValidMask_Gajalaka(Pawn p)
+        {
+            List<Apparel> wornApparel = p.apparel.WornApparel.Where(x => x.def.apparel.bodyPartGroups.Contains(BodyPartGroupDefOf.FullHead)).ToList();
+            foreach (Apparel ap in wornApparel)
+            {
+                ApparelProperties props = ApparelProperties.Get(ap.def);
+                if (props != null && props.isGajalakaMask)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static bool WearingValidMask_Shakalaka(Pawn p)
+        {
+            List<Apparel> wornApparel = p.apparel.WornApparel.Where(x => x.def.apparel.bodyPartGroups.Contains(BodyPartGroupDefOf.FullHead)).ToList();
+            foreach (Apparel ap in wornApparel)
+            {
+                ApparelProperties props = ApparelProperties.Get(ap.def);
+                if (props != null && props.isShakalakaMask)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ 1.3/Source/Mashed_Lynians/Mashed_Lynians/ThoughtWorker/Ideo/ThoughtWorker_Mask_ShakalakaThought.cs
@@ -0,0 +1,15 @@
+﻿using RimWorld;
+using Verse;
+
+namespace Mashed_Lynians
+{
+    public class ThoughtWorker_MissingMask_ShakalakaThought : ThoughtWorker_Precept
+    {
+        protected override ThoughtState ShouldHaveThought(Pawn p) => MaskThoughtUtility.Valid(p.def) && !MaskThoughtUtility.WearingValidMask_Shakalaka(p);
+    }
+
+    public class ThoughtWorker_WearingMask_ShakalakaThought : ThoughtWorker_Precept
+    {
+        protected override ThoughtState ShouldHaveThought(Pawn p) => MaskThoughtUtility.Valid(p.def) && MaskThoughtUtility.WearingValidMask_Shakalaka(p);
+    }
+}
