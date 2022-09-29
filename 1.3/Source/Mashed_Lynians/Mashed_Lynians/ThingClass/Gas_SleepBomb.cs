a628766f478fe46c6e20ce73077be7460c087e47
SirMashedPotato <88062719+SirMashedPotato@users.noreply.github.com>
2022-09-29 16:38:18 +1300

    Sleep bomb improvement

--- 1.3/Source/Mashed_Lynians/Mashed_Lynians/ThingClass/Gas_SleepBomb.cs
+++ 1.3/Source/Mashed_Lynians/Mashed_Lynians/ThingClass/Gas_SleepBomb.cs
@@ -3,6 +3,7 @@ using Verse;
 using RimWorld;
 using System.Collections.Generic;
 using UnityEngine;
+using Verse.AI;
 
 namespace Mashed_Lynians
 {
@@ -36,6 +37,15 @@ namespace Mashed_Lynians
                                     float num2 = Mathf.Lerp(0.85f, 1.15f, Rand.ValueSeeded(p.thingIDNumber ^ 74374237));
                                     num *= num2;
                                     p.needs.rest.CurLevel -= num * 3;
+
+                                    if (p.needs.rest.CurLevel <= 0 && Rand.Chance(0.25f))
+                                    {
+                                        if (p.Drafted)
+                                        {
+                                            p.drafter.Drafted = false;
+                                        }
+                                        p.jobs.StartJob(JobMaker.MakeJob(JobDefOf.LayDown, p.Position), JobCondition.InterruptForced, null, false, true, null, new JobTag?(JobTag.SatisfyingNeeds), false, false);
+                                    }
                                 }
                             }
                         }
